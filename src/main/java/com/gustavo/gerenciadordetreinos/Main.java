3c0b7a6927c4a4261b2331a0f4dbceb4413564bc
GustavoLopesMoreira <gustavolmconta@gmail.com>
2026-08-17 21:06:28 -0300

    Criação do menu interativo
    Implementação do loop principal, opções do switch-case usando Scanner e tratamento de exceções com try-catch

--- src/main/java/com/gustavo/gerenciadordetreinos/Main.java
+++ src/main/java/com/gustavo/gerenciadordetreinos/Main.java
@@ -1,7 +1,88 @@
 package com.gustavo.gerenciadordetreinos;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Projeto iniciado");
+        Scanner sc = new Scanner(System.in);
+        List<String> treinos = new ArrayList<>();
+
+        System.out.println("Gerenciador de treinos");
+        int r = 0;
+
+        do {
+            try {
+                System.out.println("1 - Lista de treinos");
+                System.out.println("2 - Criar treino");
+                System.out.println("3 - Deletar treino");
+                System.out.println("4 - Sair");
+                System.out.print("Escolha uma opção: ");
+
+                r = sc.nextInt();
+                sc.nextLine();
+
+                switch (r) {
+                    case 1:
+                        if (treinos.isEmpty()) {
+                            System.out.println("Nenhum treino criado.");
+                        } else {
+                            System.out.println("Treinos");
+                            for (String treino : treinos) {
+                                System.out.println(treino);
+                            }
+                        }
+                        break;
+
+                    case 2:
+                        System.out.print("Digite o nome do treino: ");
+                        String nomeTreino = sc.nextLine();
+                        treinos.add(nomeTreino);
+                        System.out.println("Treino criado com sucesso");
+                        break;
+
+                    case 3:
+                        if (treinos.isEmpty()) {
+                            System.out.println("Nenhum treino para deletar.");
+                        } else {
+                            System.out.println("Treinos");
+                            int i = 0;
+                            for (String treino : treinos) {
+                                System.out.printf("%d° %s\n", i + 1, treino);
+                                i++;
+                            }
+
+                            System.out.print("Digite o numero do treino a ser removido: ");
+                            int numeroRemovido = sc.nextInt();
+
+                            if (numeroRemovido >= 1 && numeroRemovido <= treinos.size()) {
+                                treinos.remove(numeroRemovido - 1);
+                                System.out.println("Treino removido com sucesso");
+                            } else {
+                                System.out.println("Valor inválido.");
+                            }
+                        }
+                        break;
+
+                    case 4:
+                        break;
+
+                    default:
+                        System.out.println("Opção inválida, tente novamente.");
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("Erro: Por favor, digite apenas números inteiros válidos.");
+                sc.nextLine();
+            } catch (Exception e) {
+                System.out.println("Ocorreu um erro inesperado: " + e.getMessage());
+            }
+
+        } while (r != 4);
+
+        System.out.println("Fim da aplicação");
+        sc.close();
     }
 }
